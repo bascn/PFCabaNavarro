9a1c250c04147fc0943be24ad8ec69e80bd98465
bascn <92053871+bascn@users.noreply.github.com>
2022-12-27 17:28:49 -0300

    Avance componentes web en SASS

--- resources/js/botonMenu.js
+++ resources/js/botonMenu.js
@@ -1,7 +1,7 @@
 // Hola, implementé este script para poder hacer funcional el botón que despliga los links en el navbar.
 
-const toggleButton = document.getElementsByClassName('navbar-toggle')[0];
-const navbarLinks = document.getElementsByClassName('navbar-link');
+const toggleButton = document.getElementsByClassName('menu-toggle')[0];
+const navbarLinks = document.getElementsByClassName('menu-link');
 
 toggleButton.addEventListener('click', function(){
     for (var index = 0; index < navbarLinks.length; index++) 
